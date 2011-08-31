45428dd3cb5f54fb2101784856bc8602af92771e
Craig Cook <boycook@me.com>
2011-08-31 15:13:06 +0100

    Adding tests for validator

--- src/test/javascript/spec/SetSpec.js
+++ src/test/javascript/spec/SetSpec.js
@@ -1,7 +1,7 @@
-describe('Set', function(){
+describe('Set', function() {
     var set;
 
-    beforeEach(function(){
+    beforeEach(function() {
         set = new Set();
         set.add('Craig Cook');
         set.add('Tim Watson');
@@ -13,47 +13,47 @@ describe('Set', function(){
         expect(set.size()).toEqual(5);
     });
 
-    it('should give the correct length', function(){
-		expect(set.size()).toEqual(5);
+    it('should give the correct length', function() {
+        expect(set.size()).toEqual(5);
     });
 
-    it('should get index correctly', function(){
+    it('should get index correctly', function() {
         expect(set.getIndex('Charles Care')).toEqual(2);
         expect(set.getIndex('Charles Doesnt Care')).toEqual(-1);
     });
 
-    it('should know if key exists', function(){
+    it('should know if key exists', function() {
         expect(set.containsKey(2)).toBeTruthy();
         expect(set.containsKey(13)).toBeFalsy();
     });
 
-    it('should know if item exists', function(){
+    it('should know if item exists', function() {
         expect(set.contains('Charles Care')).toBeTruthy();
         expect(set.contains('Charles doesnt Care')).toBeFalsy();
     });
 
-    it('should be able to remove item', function(){
-		set.remove('Charles Care');
-		expect(set.size()).toEqual(4);
-		expect(set.contains('Charles Care')).toBeFalsy();
+    it('should be able to remove item', function() {
+        set.remove('Charles Care');
+        expect(set.size()).toEqual(4);
+        expect(set.contains('Charles Care')).toBeFalsy();
     });
 
-    it('should not add item twice', function(){
-		set.add('Charles Care');
-		expect(set.size()).toEqual(5);
-		expect(set.contains('Charles Care')).toBeTruthy();
+    it('should not add item twice', function() {
+        set.add('Charles Care');
+        expect(set.size()).toEqual(5);
+        expect(set.contains('Charles Care')).toBeTruthy();
     });
 
-    it('should add all correctly', function(){
-		set.addAll(['Foo bar', 'John Smith', 'Davy Jones']);
-		expect(set.size()).toEqual(8);
-		expect(set.contains('Foo bar')).toBeTruthy();
+    it('should add all correctly', function() {
+        set.addAll(['Foo bar', 'John Smith', 'Davy Jones']);
+        expect(set.size()).toEqual(8);
+        expect(set.contains('Foo bar')).toBeTruthy();
         expect(set.contains('John Smith')).toBeTruthy();
         expect(set.contains('Davy Jones')).toBeTruthy();
     });
 
-    it('should clear properly', function(){
-		set.clear();
-		expect(set.size()).toEqual(0);
+    it('should clear properly', function() {
+        set.clear();
+        expect(set.size()).toEqual(0);
     });
 });
---
+++ src/test/javascript/spec/ValidatorSpec.js
@@ -0,0 +1,51 @@
+describe('Validator', function() {
+
+    it('should validate required correctly', function() {
+        checkValidation('some value', 'required', true);
+        checkValidation('', 'required', false);
+    });
+
+    it('should validate-dd correctly', function() {
+        checkValidation('some value', 'validate-dd', true);
+        checkValidation('', 'validate-dd', false);
+        checkValidation('-1', 'validate-dd', false);
+    });
+
+    it('should validate-version correctly', function() {
+        checkValidation('v1.0.0.0', 'validate-version', true);
+        checkValidation('v12.10.11.09', 'validate-version', true);
+        checkValidation('v1.0.0', 'validate-version', false);
+    });
+
+    it('should validate-ein correctly', function() {
+        checkValidation('702242036', 'validate-ein', true);
+        checkValidation('7022420366', 'validate-ein', false);
+        checkValidation('70224203', 'validate-ein', false);
+        checkValidation('', 'validate-ein', false);
+    });
+
+    it('should validate-email correctly', function() {
+        checkValidation('craig.cook@cook.com', 'validate-email', true);
+        checkValidation('craig@cook.com', 'validate-email', true);
+        checkValidation('@cook.com', 'validate-email', false);
+        checkValidation('craig@', 'validate-email', false);
+        checkValidation('', 'validate-email', false);
+    });
+
+    it('should validate-url correctly', function() {
+        checkValidation('http://www.craigcook.co.uk', 'validate-url', true);
+        checkValidation('http://craigcook.co.uk', 'validate-url', true);
+        checkValidation('www.craigcook.co.uk', 'validate-url', false);
+        checkValidation('craigcook.co.uk', 'validate-url', false);
+        checkValidation('', 'validate-url', false);
+    });
+
+    function checkValidation(value, ruleName, expected) {
+        var rule = $.validate.rules[ruleName];
+        if (expected) {
+            expect($.validate.checkValue(value, rule.pattern)).toBeTruthy();
+        } else {
+            expect($.validate.checkValue(value, rule.pattern)).toBeFalsy();
+        }
+    }
+});
