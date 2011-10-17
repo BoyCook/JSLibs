3d928015361e906a780e8a1ec728f975a4ad08ca
Craig Cook <boycook@me.com>
2011-10-17 13:25:32 +0100

    Updated collapsible pluging to not require div id

--- src/main/javascript/jquery.collapsible.js
+++ src/main/javascript/jquery.collapsible.js
@@ -1,56 +1,70 @@
 /**
- * @groupId 	>= org.cccs.jsLibs
- * @artefactId 	>= jquery.collapsible
- * @version   	>= 1.0
- *	
- * @name 		>= jquery.collapsible
+ * @name        >= jquery.collapsible
+ * @version     >= 1.0
  * @description >= Makes a div collapsible, similar to an accordion
- * @vcs			>= git
- * @website		>= https://github.com/BoyCook/JSLibs
- * @since     	>= 2010-07-01
- * @copyright 	>= Copyright (c) 2010 CCCS Ltd. http://craigcook.co.uk
- * @author    	>= Craig Cook
- * @requires  	>= jQuery 1.4.2           http://jquery.com
+ * @vcs         >= git
+ * @website     >= https://github.com/BoyCook/JSLibs
+ * @since       >= 2010-07-01
+ * @copyright   >= Copyright (c) 2010 CCCS Ltd. http://craigcook.co.uk
+ * @author      >= Craig Cook
+ * @requires    >= jQuery 1.4.2           http://jquery.com
  */
 
 (function($) {
-    $.fn.collapsible = function(open) {
-        if (open == undefined) {
-            open = false;
-        }
-
-        return this.each(function(){
-            var element = '#' + this.id;
-            var divCss = 'ui-accordion ui-widget ui-helper-reset ui-accordion-icons collapsible-div';
-            var collapsedCss = 'ui-accordion-header ui-helper-reset collapsible-head ui-state-default ui-corner-all';
-            var expandedCss = 'ui-accordion-header ui-helper-reset collapsible-head ui-state-active ui-corner-top';
-            var text = $(element + ' h3:first').text();
-			var doOpen = $(element).hasClass('collapsible-open') || open;
-
-            $(element).addClass(divCss);
-            $(element + ' h3:first').html("<span class=\"ui-icon ui-icon-triangle-1-e\"></span><a href=\"#\">" + text + "</a>");
-
-            if (doOpen) {
-                $(element + ' h3:first').attr('class', expandedCss);
-                $(element + ' span:first').attr('class', 'ui-icon ui-icon-triangle-1-s');
-                $(element + ' div:first').show();
+    var config = {
+        containerCss: 'ui-accordion ui-widget ui-helper-reset ui-accordion-icons collapsible-div',
+        headCssExpanded: 'ui-accordion-header ui-helper-reset collapsible-head ui-state-active ui-corner-top',
+        headCssCollapsed: 'ui-accordion-header ui-helper-reset collapsible-head ui-state-default ui-corner-all',
+        spanCssExpanded: 'ui-icon ui-icon-triangle-1-s',
+        spanCssCollapsed: 'ui-icon ui-icon-triangle-1-e'
+    };
+    var methods = {
+        toggle: function(element) {
+            if ($(element).children('div:first').is(':visible')) {
+                methods.collapse(element);
             } else {
-                $(element + ' h3:first').attr('class', collapsedCss);
-                $(element + ' div:first').hide();
+                methods.open(element);
             }
-
-            $(element + ' h3:first').click(function() {
-                if ($(element + ' div:first').is(':visible')) {
-                    $(element + ' h3:first').attr('class', collapsedCss);
-                    $(element + ' span:first').attr('class', 'ui-icon ui-icon-triangle-1-e');
-                } else {
-                    $(element + ' h3:first').attr('class', expandedCss);
-                    $(element + ' span:first').attr('class', 'ui-icon ui-icon-triangle-1-s');
-                }
-
-                $(element + ' div:first').toggle();
-                return false;
+        },
+        open: function(element) {
+            $(element).children('h3:first').attr('class', config.headCssExpanded);
+            $(element).children('h3:first').children('span:first').attr('class', config.spanCssExpanded);
+            $(element).children('div:first').show();
+        },
+        collapse: function(element) {
+            $(element).children('h3:first').attr('class', config.headCssCollapsed);
+            $(element).children('h3:first').children('span:first').attr('class', config.spanCssCollapsed);
+            $(element).children('div:first').hide();
+        },
+        makeCollapsible: function(element, open) {
+            var text = $(element).children('h3:first').text();
+            var doOpen = $(element).hasClass('collapsible-open') || open;
+            $(element).addClass(config.containerCss);
+            $(element).children('h3:first').html("<span class=\"ui-icon ui-icon-triangle-1-e\"></span><a href=\"#\">" + text + "</a>");
+            if (doOpen) {
+                methods.open(element);
+            } else {
+                methods.collapse(element);
+            }
+            $(element).children('h3:first').click(function() {
+                methods.toggle(element);
             });
-        });
+        }
+    };
+
+    $.fn.collapsible = function(method) {
+        if (methods[method]) {
+            this.each(function() {
+                methods[method].call(this, this);
+            });
+        } else if (typeof method === 'boolean' || !method) {
+            //method may be undefined
+            this.each(function() {
+                methods.makeCollapsible(this, (!method ? false : true));
+            });
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.validate');
+        }
+        return this;
     };
 })(jQuery);
