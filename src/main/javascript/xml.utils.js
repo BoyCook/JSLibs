d458ff09b193bfb165ccde0cc30671ed88656859
Craig Cook <boycook@me.com>
2011-12-16 16:20:55 +0000

    Removing asset based xml functions

--- src/main/javascript/xml.utils.js
+++ src/main/javascript/xml.utils.js
@@ -31,31 +31,6 @@ function getCDATAValue(cdata) {
     return value;
 }
 
-function getAsset(xml) {
-    return {
-        name: xml.getAttribute('displayName'),
-        id: xml.getAttribute('identity'),
-        version: xml.getAttribute('version')
-    };
-}
-
-function readFormToXML(id, xml) {
-    var root = $(xml.childNodes[0]).children()[0];
-    $('#' + id + ' .attribute-box, #' + id + ' .attribute-disabled').each(function() {
-        if ($(this).hasClass('attribute-ein')) {
-            var node = getChildNode(root, this.id);
-            var einNode = $(node).find('ein:first');
-            einNode.text(this.value);
-        } else if ($(this).hasClass('attribute-boolean')) {
-        	var val = $('#' + this.id).is(':checked');
-        	// FIX: IE8 seems to convert false and true to 0 & -1 - so passing it as 1 & 0 instead
-            setXMLValue(root, this.id, val?'1':'0');
-        } else {
-            setXMLValue(root, this.id, this.value);
-        }
-    });
-}
-
 function setXMLValue(xml, nodeName, value) {
     var node = getChildNode(xml, nodeName);
     $(node).text(value);
